a1f39770af2bcbe33797982d561de13d7653dbcf
Sujal Chauhan <chauhansujal1107@gmail.com>
2024-09-12 20:54:53 +0530

    added linktree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ const Home: React.FC = () => {
     <div className="text-center ">
       <h1 className="text-4xl font-bold mb-4">Welcome to the Feedback App</h1>
       <p className="text-xl">Choose an option from the navbar to get started.</p>
+      <p>Connect With me : </p>
+      <a href="https://www.linktr.ee/chauhansujal07/" className="text-blue-400">Linktree</a>
     </div>
   );
 };
